7d9aaadb22ed7984460df2ed85a87970eb1fb804
小法 <x@q.com>
2024-12-04 11:59:06 +0800

    更新适配

--- src/views/home/home-com/winLoop.vue
+++ src/views/home/home-com/winLoop.vue
@@ -1,12 +1,15 @@
 <template>
   <div style="width: 100%; height: 100%; position: relative; overflow: hidden">
-    <div class="trans-box" :style="{ 'transform': `translateY(-${topNum * (conf.totalLength - 4)}px)` }">
+    <div
+      class="trans-box"
+      :style="{ 'transform': `translateY(-${sutil.px2rem(props.topNum * (conf.totalLength - 4))}rem)` }"
+    >
       <template v-for="(key, index) in conf.dataKeys" :key="index">
         <div
           style="position: absolute; left: 0; right: 0"
           :style="{
             transition: conf.data[key].style.transition,
-            top: conf.data[key].style.top + 'px'
+            top: sutil.px2rem(conf.data[key].style.top) + 'rem'
           }"
         >
           <slot :num="conf.data[key].data?.id" :topNum="conf.data[key].style.top"></slot>
@@ -17,8 +20,8 @@
 </template>
 
 <script setup lang="ts">
+import sutil from '@/sstore/sutil'
 import { onMounted, onUnmounted, reactive } from 'vue'
-
 const props = defineProps({
   winList: { default: [] as any[] },
   topNum: { default: 65 }
@@ -45,7 +48,7 @@ const conf = reactive({
       })
     }
 
-    if(conf.totalLength > conf.dataSource.length){
+    if (conf.totalLength > conf.dataSource.length) {
       conf.totalLength = conf.dataSource.length
     }
     for (let i = 0; i < conf.totalLength + 1; i++) {
