329adfc13cb82f358143b9bee3d01b0d7697b2fb
小法 <x@q.com>
2024-12-10 17:44:45 +0800

    更新发送列表

---
+++ src/views/chat/chating/message/item.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+    <div class="face">
+      <img :src="item.face" />
+    </div>
+    <div class="item-content">
+      <div v-if="item.type == 'text'" v-html="item.content"></div>
+      <div v-else-if="item.type == 'img'">
+        <img :src="item.content" />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { reactive } from 'vue'
+defineProps<{
+  item: { type: string; content: string; sendId: string; receiveId: string; face: string }
+}>()
+const conf = reactive({})
+</script>
+<style lang="less" scoped></style>
--- src/views/chat/chating/message/list.vue
+++ src/views/chat/chating/message/list.vue
@@ -1,15 +1,11 @@
 <template>
   <div v-for="item in mconf.chat.list" :key="item">
-    <div v-if="item.type == 'text'" v-html="item.content"></div>
-    <div v-else-if="item.type == 'img'">
-      <img :src="item.content" />
-    </div>
+    <Item :item="item" />
   </div>
 </template>
 <script setup lang="ts">
 import { Scope } from 'tools-vue3'
-import { reactive } from 'vue'
+import Item from './item.vue'
 const mconf = Scope.getConf()
-const conf = reactive({})
 </script>
 <style lang="less" scoped></style>
