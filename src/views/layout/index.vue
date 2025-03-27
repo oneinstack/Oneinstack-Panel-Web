ace9aa8bd929b6575c59ae744e7368b0f86fdbc6
zgl <2516195124@qq.com>
2025-03-27 11:32:11 +0800

    style:layout缺失图标添加

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -61,15 +61,15 @@ const conf = reactive({
         dark: ['#eab170', '#ffffff']
       }
     },
-    // {
-    //   name: '监控',
-    //   path: '/monitor',
-    //   icon: 'monitor',
-    //   activeColor: {
-    //     light: ['#eab170', '#8B8B8B'],
-    //     dark: ['#eab170', '#ffffff']
-    //   }
-    // },
+    {
+      name: '监控',
+      path: '/monitor',
+      icon: 'monitor',
+      activeColor: {
+        light: ['#eab170', '#8B8B8B'],
+        dark: ['#eab170', '#ffffff']
+      }
+    },
     {
       name: '安全',
       path: '/security',
@@ -88,24 +88,24 @@ const conf = reactive({
         dark: ['#eab170', '#ffffff']
       }
     },
-    // {
-    //   name: '日志',
-    //   path: '/log',
-    //   icon: 'log',
-    //   activeColor: {
-    //     light: ['#eab170', '#8B8B8B'],
-    //     dark: ['#eab170', '#ffffff']
-    //   }
-    // },
-    // {
-    //   name: '终端',
-    //   path: '/terminal',
-    //   icon: 'terminal',
-    //   activeColor: {
-    //     light: ['#8B8B8B', '#eab170'],
-    //     dark: ['#ffffff', '#eab170']
-    //   }
-    // },
+    {
+      name: '日志',
+      path: '/log',
+      icon: 'log',
+      activeColor: {
+        light: ['#eab170', '#8B8B8B'],
+        dark: ['#eab170', '#ffffff']
+      }
+    },
+    {
+      name: '终端',
+      path: '/terminal',
+      icon: 'terminal',
+      activeColor: {
+        light: ['#8B8B8B', '#eab170'],
+        dark: ['#ffffff', '#eab170']
+      }
+    },
     {
       name: '计划任务',
       path: '/task',
