0592f9713b6bfa7627f80e288b847d98833d278a
zgl <2516195124@qq.com>
2026-08-10 11:42:46 +0800

    优化：统一表单日期和数字输入样式

--- src/views/pages/task/components/add-task.vue
+++ src/views/pages/task/components/add-task.vue
@@ -729,19 +729,6 @@ const handleScriptInput = (event: Event) => {
 
 .task-drawer-form {
   max-width: 680px;
-
-  :deep(.el-input-number__increase),
-  :deep(.el-input-number__decrease) {
-    display: none;
-  }
-
-  :deep(.el-input-number .el-input__wrapper) {
-    padding-right: 11px;
-  }
-
-  :deep(.el-input-number .el-input__inner) {
-    text-align: left;
-  }
 }
 
 .cycle-row {
